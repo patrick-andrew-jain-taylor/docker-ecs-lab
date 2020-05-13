7e9a6ad5c54c7533efc1e0af7fe8624d8094f436
Patrick Jain-Taylor <pjaintaylor@eatonvance.com>
2020-05-13 12:22:13 -0400

    Adding ECS cluster CFT.

---
+++ ecs_cluster.tf
@@ -0,0 +1,9 @@
+resource "aws_cloudformation_stack" "ecs_cluster" {
+  name = "ecs-cluster"
+  template_url = "https://dev-day-bucket.s3-ap-southeast-2.amazonaws.com/lab-templates/1_ClusterTemplateV3.template"
+  parameters {
+    AsgMaxSize = 2
+    ECSAMI = "/aws/service/ecs/optimized-ami/amazon-linux/recommended/image_id"
+  }
+  capabilities = ["CAPABILITY_IAM"]
+}
