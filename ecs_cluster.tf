0452eca26681e517c2d71db269fc57156d86707f
Patrick Jain-Taylor <pjaintaylor@eatonvance.com>
2020-05-13 12:24:01 -0400

    Fixing typo.

--- ecs_cluster.tf
+++ ecs_cluster.tf
@@ -1,7 +1,7 @@
 resource "aws_cloudformation_stack" "ecs_cluster" {
   name = "ecs-cluster"
   template_url = "https://dev-day-bucket.s3-ap-southeast-2.amazonaws.com/lab-templates/1_ClusterTemplateV3.template"
-  parameters {
+  parameters = {
     AsgMaxSize = 2
     ECSAMI = "/aws/service/ecs/optimized-ami/amazon-linux/recommended/image_id"
   }
